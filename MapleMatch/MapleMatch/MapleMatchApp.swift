75bda18f0ee0822bb1cea1faa4be5dbaf3f3e8ad
Xyd Espino <xydespino@outlook.com>
2024-01-30 14:26:32 -0500

    Lecture 1: The Basics of SwiftUI

---
+++ MapleMatch/MapleMatch/MapleMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapleMatchApp.swift
+//  MapleMatch
+//
+//  Created by Xyd Espino on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct MapleMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
